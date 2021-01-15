c75f3561dd0a033554730aab49e97e99fa0f36b6
Jordon Smith <jordon00smith@gmail.com>
2021-01-15 14:20:24 -0700

    Display team infor on main page

--- src/TeamStats/TeamStats.js
+++ src/TeamStats/TeamStats.js
@@ -1,11 +1,35 @@
 import './TeamStats.css'
+import { Component } from 'react'
+import { getTeamStats } from '../apiCalls'
 
-const TeamStats = () => {
-  return(
-    <section className='team-stats'>
-      <p>Mavs team stats go here</p>
-    </section>
-  )
+class TeamStats extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fullName: '',
+      conference: '',
+      division: '',
+    }
+  }
+
+  componentDidMount = async () => {
+    await getTeamStats(7)
+    .then(data => this.setState({
+      fullName: data.full_name, 
+      conference: data.conference, 
+      division: data.division 
+    }))
+  }
+
+  render() {
+    return(
+      <section className='team-stats'>
+        <p>Team Name: {this.state.fullName}</p>
+        <p>Conference: {this.state.conference}</p>
+        <p>Division: {this.state.division}</p>
+      </section>
+    )
+  }
 }
 
 export default TeamStats
