100fdc16bc609ae301b29af771bbd764f5e1ce8d
Jordon Smith <jordon00smith@gmail.com>
2021-01-15 17:08:35 -0700

    Get all data to appear on home page

--- src/PlayersList/PlayersList.js
+++ src/PlayersList/PlayersList.js
@@ -6,7 +6,76 @@ class PlayersList extends Component {
   constructor() {
     super()
     this.state = {
-      players: [132, 378]
+      players: [
+        {
+          name: 'Luka Doncic',
+          id: 132
+        },
+        {
+          name: 'Kristaps Porzingis',
+          id: 378
+        },
+        {
+          name: 'Jalen Brunson',
+          id: 73
+        },
+        {
+          name: 'Tim Hardaway Jr.',
+          id: 191
+        },
+        {
+          name: 'Josh Richardson',
+          id: 391
+        },
+        {
+          name: 'Maxi Kleber',
+          id: 257
+        },
+        {
+          name: 'Dwight Powell',
+          id: 379
+        },
+        {
+          name: 'Willie Cauley-Stein',
+          id: 91
+        },
+        {
+          name: 'Trey Burke',
+          id: 76
+        },
+        {
+          name: 'James Johnson',
+          id: 242
+        },
+        {
+          name: 'Josh Green',
+          id: 3547258
+        },
+        {
+          name: 'Dorian Finney-Smith',
+          id: 158
+        },
+        {
+          name: 'Tyrell Terry',
+          id: 3547255
+        },
+        {
+          name: 'Tyler Bey',
+          id: 3547265
+        },
+        {
+          name: 'Wes Iwundu',
+          id: 230
+        },
+        {
+          name: 'Nate Hinton',
+          id: 3547281
+        },
+        {
+          name: 'Boban Marjanovic',
+          id: 296
+        }
+      ]
     }
   }
 
@@ -14,7 +83,7 @@ class PlayersList extends Component {
     return(
       <section className='players-list'>
         {this.state.players.map(player => {
-          return <PlayerCard id={player} key={player}/>
+          return <PlayerCard id={player.id} key={player.id}/>
         })}
       </section>
     )
