4a2fbf74a2fbb30e6d1c544c2b06b653ea5abbd5
Jordon Smith <jordon00smith@gmail.com>
2021-01-15 21:57:58 -0700

    Add in players ids

--- src/PlayersList/PlayersList.js
+++ src/PlayersList/PlayersList.js
@@ -8,72 +8,72 @@ class PlayersList extends Component {
     this.state = {
       players: [
         {
-          name: 'Luka Doncic',
-          id: 132
-        },
-        {
-          name: 'Kristaps Porzingis',
-          id: 378
+          name: 'Tyler Bey',
+          id: 3547265
         },
         {
           name: 'Jalen Brunson',
           id: 73
         },
         {
-          name: 'Tim Hardaway Jr.',
-          id: 191
-        },
-        {
-          name: 'Josh Richardson',
-          id: 391
-        },
-        {
-          name: 'Maxi Kleber',
-          id: 257
-        },
-        {
-          name: 'Dwight Powell',
-          id: 379
+          name: 'Trey Burke',
+          id: 76
         },
         {
           name: 'Willie Cauley-Stein',
           id: 91
         },
         {
-          name: 'Trey Burke',
-          id: 76
-        },
-        {
-          name: 'James Johnson',
-          id: 242
-        },
-        {
-          name: 'Josh Green',
-          id: 3547258
+          name: 'Luka Doncic',
+          id: 132
         },
         {
           name: 'Dorian Finney-Smith',
           id: 158
         },
         {
-          name: 'Tyrell Terry',
-          id: 3547255
+          name: 'Josh Green',
+          id: 3547258
         },
         {
-          name: 'Tyler Bey',
-          id: 3547265
-        },
-        {
-          name: 'Wes Iwundu',
-          id: 230
+          name: 'Tim Hardaway Jr.',
+          id: 191
         },
         {
           name: 'Nate Hinton',
           id: 3547281
         },
         {
+          name: 'Wes Iwundu',
+          id: 230
+        },
+        {
+          name: 'James Johnson',
+          id: 242
+        },
+        {
+          name: 'Maxi Kleber',
+          id: 257
+        },
+        {
           name: 'Boban Marjanovic',
           id: 296
+        },
+        {
+          name: 'Kristaps Porzingis',
+          id: 378
+        },
+        {
+          name: 'Dwight Powell',
+          id: 379
+        },
+        {
+          name: 'Josh Richardson',
+          id: 391
+        },
+        {
+          name: 'Tyrell Terry',
+          id: 3547255
         }
       ]
     }
