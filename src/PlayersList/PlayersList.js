986fdff22277b25cc326ff9b121bd3daea39447d
Jordon Smith <jordon00smith@gmail.com>
2021-01-16 15:23:47 -0700

    Convert PlayersList from class component to functional component

--- src/PlayersList/PlayersList.js
+++ src/PlayersList/PlayersList.js
@@ -1,21 +1,14 @@
 import './PlayersList.css'
-import { Component } from 'react'
 import PlayerCard from '../PlayerCard/PlayerCard'
 
-class PlayersList extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return(
-      <section className='players-list'>
-        {this.props.players.map(player => {
-          return <PlayerCard id={player.id} key={player.id}/>
-        })}
-      </section>
-    )
-  }
+const PlayersList = (props) => {
+  return(
+    <section className='players-list'>
+      {props.players.map(player => {
+        return <PlayerCard id={player.id} key={player.id}/>
+      })}
+    </section>
+  )
 }
 
 export default PlayersList
